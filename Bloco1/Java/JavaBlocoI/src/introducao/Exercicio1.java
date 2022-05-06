af0e590d3dbee417a96a780baade8c339232b6ae
PatriciAlves <patricia.18_08@hotmail.com>
2022-05-06 13:24:57 -0300

    Bloco I Introducao a Java

---
+++ Bloco1/Java/JavaBlocoI/src/introducao/Exercicio1.java
@@ -0,0 +1,30 @@
+package introducao;
+
+import java.util.Scanner;
+//Faça um programa que receba três inteiros e diga qual deles é o maior.
+
+public class Exercicio1 {
+	
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int a, b, c;
+
+		System.out.println("Digite o primeiro valor: ");
+		a = ler.nextInt();
+		System.out.println("Digite o primeiro valor: ");
+		b = ler.nextInt();
+		System.out.println("Digite o primeiro valor: ");
+		c = ler.nextInt();
+		
+		if(a>=b && a>=c) {
+			System.out.println("O maior numero digitado foi: " + a);
+			
+		}else if(b>=a && b>=c){
+			System.out.println("O maior numero digitado foi: " + b);
+			
+		}else {
+			System.out.println("O maior numero digitado foi: " + c);
+		}
+		
+	}
+}
---
+++ Bloco1/Java/JavaBlocoI/src/introducao/Exercicio4.java
@@ -0,0 +1,25 @@
+package introducao;
+
+import java.util.Scanner;
+
+/*Faça um programa em que permita a entrada de um número qualquer e exiba se este
+número é par ou ímpar. Se for par exiba também a raiz quadrada do mesmo; se for
+ímpar exiba o número elevado ao quadrado.*/
+
+
+public class Exercicio4 {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int n;
+		
+		System.out.println("Digite um numero inteiro: ");
+		n=ler.nextInt();
+		
+		if(n%2 == 1) {
+			System.out.println("IMPAR");
+		}else {
+			System.out.println("PAR");
+		}
+	}
+
+}
