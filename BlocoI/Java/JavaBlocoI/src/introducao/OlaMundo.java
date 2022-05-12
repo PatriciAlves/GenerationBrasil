880fecb76cbc34982e4f9f72624d676441873d02
PatriciAlves <patricia.18_08@hotmail.com>
2022-05-12 16:26:38 -0300

    Introdução a Herança e Polimorfismo

--- BlocoI/Java/JavaBlocoI/src/introducao/OlaMundo.java
+++ BlocoI/Java/JavaBlocoI/src/introducao/OlaMundo.java
@@ -2,9 +2,22 @@ package introducao;
 
 public class OlaMundo {
 	
-	public static void main(String[] args) {
-		
-		System.out.println("Hello Word");
-	}
+	
+
+	    public static void main(String[] args) {
+
+	        double salario = 5300.0;
+
+	        if(salario < 2600.0) {
+	            System.out.println("A sua aliquota é de 15%");
+	            System.out.println("Você pode deduzir até R$ 350");
+	        }
+
+	         if(salario > 3750.0) {
+	            System.out.println("A sua aliquota é de 22,5%");
+	            System.out.println("Você pode deduzir até R$ 636");
+	        }
+	    }    
+	
 
 }
