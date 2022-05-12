f9485441b9017b0fc3baa57a1040f1b5770c20f9
PatriciAlves <patricia.18_08@hotmail.com>
2022-05-12 14:38:49 -0300

    Introdução a Herança e Polimorfismo

--- BlocoI/Java/JavaBlocoI/src/IntroducaoHeranca/Exercicio1/Animal.java
+++ BlocoI/Java/JavaBlocoI/src/IntroducaoHeranca/Exercicio1/Animal.java
@@ -3,7 +3,7 @@ package IntroducaoHeranca.Exercicio1;
 public class Animal {
 
 	private String nome;
-	private int idade;
+	
 	
 
 	public Animal() {
@@ -14,7 +14,7 @@ public class Animal {
 	public Animal(String nome, int idade) {
 		super();
 		this.nome = nome;
-		this.idade = idade;
+		
 	}
 
 	public String getNome() {
@@ -25,13 +25,6 @@ public class Animal {
 		this.nome = nome;
 	}
 
-	public int getIdade() {
-		return idade;
-	}
-
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
 
 	
 
