5a5b4cae52488cb9224f9cf6133e2b11298b37fb
PatriciAlves <patricia.18_08@hotmail.com>
2022-05-30 17:17:37 -0300

    Atividade CRUD Back-end blog-pessoal

--- Spring Bloco II Blog Pessoal/blogpessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ Spring Bloco II Blog Pessoal/blogpessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -2,30 +2,74 @@ package com.generation.blogpessoal.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
 
-
-
 @RestController
 @RequestMapping("/postagens")
-@CrossOrigin(origins = "*", allowedHeaders = "*") //habilita as requisições do front
+@CrossOrigin(origins = "*", allowedHeaders = "*") // habilita as requisições do front
 public class PostagemController {
-	
+
 	@Autowired
 	private PostagemRepository postagemRepository;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Postagem>>getAll(){
-		return ResponseEntity.ok(postagemRepository.findAll()); //select * from tb_postagens;
+	public ResponseEntity<List<Postagem>> getAll() {
+		return ResponseEntity.ok(postagemRepository.findAll()); // select * from tb_postagens;
 	}
-	
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable Long id) {
+		return postagemRepository.findById(id) // expressão lambda que trata a resposta
+				.map(resposta -> ResponseEntity.ok(resposta)).orElse(ResponseEntity.notFound().build());
+	}
+
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo) {
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+
+		// select*from tb_postagens where titulo like "%titulo%";
+	}
+
+	@PostMapping
+	public ResponseEntity<Postagem> putPostagem(@Valid @RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+	}
+
+	@PutMapping
+	public ResponseEntity<?> postPostagem(@Valid @RequestBody Postagem postagem) {
+		if (postagem.getId() == null) {
+			return new ResponseEntity<String>("Porfavor informe o ID da postagem que deseja atualizar",
+					HttpStatus.BAD_REQUEST);
+		} else {
+
+			return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
+		}
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deletePostagem(@PathVariable Long id) {
+		return postagemRepository.findById(id).map(resposta -> {
+			postagemRepository.deleteById(id);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+
+		}).orElse(ResponseEntity.notFound().build());
+
+	}
 
 }
