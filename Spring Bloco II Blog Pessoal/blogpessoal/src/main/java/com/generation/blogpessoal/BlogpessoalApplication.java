925fd24136a9793de310988a7bda07d3615a101f
PatriciAlves <patricia.18_08@hotmail.com>
2022-06-08 16:12:56 -0300

    Spring Security

--- Spring Bloco II Blog Pessoal/blogpessoal/src/main/java/com/generation/blogpessoal/BlogpessoalApplication.java
+++ Spring Bloco II Blog Pessoal/blogpessoal/src/main/java/com/generation/blogpessoal/BlogpessoalApplication.java
@@ -2,7 +2,10 @@ package com.generation.blogpessoal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+
+@ComponentScan(basePackages = {"com.generation.blogpessoal.service"})
 @SpringBootApplication
 public class BlogpessoalApplication {
 
--- Spring Bloco II Blog Pessoal/blogpessoal/src/main/java/com/generation/blogpessoal/model/Tema.java
+++ Spring Bloco II Blog Pessoal/blogpessoal/src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -14,6 +14,9 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import lombok.Data;
+
+@Data
 @Entity
 @Table(name = "tb_temas")
 public class Tema {
@@ -34,35 +37,6 @@ public class Tema {
 	
 	
 	
-
-	public Long getId() {
-		return id;
-	}
-
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
-
-	public List<Postagem> getPostagem() {
-		return postagem;
-	}
-
-
-	public void setPostagem(List<Postagem> postagem) {
-		this.postagem = postagem;
-	}
 
 	
 }
